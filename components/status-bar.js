27eed7782cd0dc82f5929b75ef6e42915e85e2d6
BaluMurugesan24 <bamurugesan@omnex.com>
2024-12-30 13:38:51 +0530

    code removed

--- components/status-bar.js
+++
@@ -1,9 +0,0 @@
-import { COLORS } from 'constants/theme-constants';
-import React from 'react';
-import { StatusBar } from 'react-native';
-
-const StatusBarAndroidIOS = ({ backgroundColor = COLORS.transparent, animated = true }) => {
-    return <StatusBar backgroundColor={backgroundColor} barStyle="dark-content" animated={animated} translucent />;
-};
-
-export default StatusBarAndroidIOS;
--- screens/splash-screen/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SplashScreenFunctional from './components/splash-screen-functional';
-
-const SplashScreen = ({ params }) => <SplashScreenFunctional />;
-
-export default SplashScreen;
