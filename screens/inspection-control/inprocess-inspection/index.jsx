287a379024c86cf1a94dee66ace3834d66a77439
BaluMurugesan24 <bamurugesan@omnex.com>
2025-07-25 16:26:54 +0530

    site id and sample size re comfirm popup added.

--- screens/inspection-control/inprocess-inspection/index.jsx
+++ screens/inspection-control/inprocess-inspection/index.jsx
@@ -800,6 +800,30 @@ const InprocessInspection = ({ route }) => {
                     }}
                 />
             )}
+            {Boolean(finalConfirmation) && (
+                <ConfirmationModal
+                    visible={finalConfirmation}
+                    handleClose={() => {
+                        setUserUpdateValue(pre => ({
+                            ...pre,
+                            CSampleSize: selectedData.CSampleSize,
+                            CHighValue: selectedData.CHighValue,
+                            CLowValue: selectedData.CLowValue,
+                            CTolerance: selectedData.CTolerance,
+                        }));
+                        setFinalConfirmation(false);
+                        setTypeOfModal('');
+                    }}
+                    content={`You've already entered ${
+                        masterData.filter(x => x?.value != '')?.length || ''
+                    } samples value. Do you want to continue and change it`}
+                    handleYesPress={() => {
+                        handleFinalConfirmYesPress();
+                    }}
+                    typeOfModal={typeOfModal}
+                    showType={false}
+                />
+            )}
         </CustomHeader>
     );
 };
