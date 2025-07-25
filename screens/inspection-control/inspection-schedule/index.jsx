6851d4073e77135194f2f4770a9bd82fc23c59d8
BaluMurugesan24 <bamurugesan@omnex.com>
2025-07-25 12:57:21 +0530

    inspection schedule page and operator worksheet SQLite.

--- screens/inspection-control/inspection-schedule/index.jsx
+++ screens/inspection-control/inspection-schedule/index.jsx
@@ -177,6 +177,7 @@ const InspectionSchedule = () => {
     useEffect(() => {
         if (icUserData && isFocused) {
             console.log('icUserData',icUserData)
+            // hnadleGetSQliteList(icUserData?.userData?.UserId,icUserData?.userData?.Siteid)
             handleListFetch(null, true, filterData.type);
         }
         return () => {
