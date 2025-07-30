2c5871a89fd465998e24763fbcc75367bed494f3
BaluMurugesan24 <bamurugesan@omnex.com>
2025-07-30 12:45:51 +0530

    SQLite added.

--- screens/inspection-control/inprocess-inspection/index.jsx
+++ screens/inspection-control/inprocess-inspection/index.jsx
@@ -67,6 +67,7 @@ const InprocessInspection = ({ route }) => {
     });
     const [finalConfirmation, setFinalConfirmation] = useState(false);
     const [showFileModal, setShowFileModal] = useState(false);
+
     const flatListRef = useRef(null);
     const navigation = useNavigation();
     const dispatch = useDispatch();
