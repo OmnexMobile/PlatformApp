3f688e1e39ce65b78efd909a0abeeb0b43469c6c
BaluMurugesan24 <bamurugesan@omnex.com>
2025-10-15 15:58:14 +0530

    bug fixed

--- screens/inspection-control/Components/RadioButtonComponent.jsx
+++ screens/inspection-control/Components/RadioButtonComponent.jsx
@@ -14,11 +14,12 @@ const RadioButtonComponent = ({
     selectedSize = 15,
     textSize = 19,
 }) => {
+    console.log(value,lable,staticValue, value == lable, staticValue == value,'valuevalue')
     const { theme } = useTheme();
     return (
         <TouchableOpacity onPress={() => onChange(obj)} activeOpacity={0.8} style={styles.container}>
             <View style={[styles.radioCircle, { borderColor: theme.colors.primaryThemeColor, height: size, width: size }]}>
-                {(value === lable || staticValue == value) && (
+                {(value == lable || staticValue == value) && (
                     <View
                         style={[styles.selectedRb, { backgroundColor: theme.colors.primaryThemeColor, width: selectedSize, height: selectedSize }]}
                     />
--- screens/inspection-control/search-inspection/index.jsx
+++ screens/inspection-control/search-inspection/index.jsx
@@ -69,21 +69,6 @@ const searchFilterList = [
         isSelected: false,
         label: 'Lot No',
     },
-    // {
-    //     id: 5,
-    //     isSelected: false,
-    //     label: 'Lot Size',
-    // },
-    // {
-    //     id: 6,
-    //     isSelected: false,
-    //     label: 'Sample Frequency',
-    // },
-    // {
-    //     id: 7,
-    //     isSelected: false,
-    //     label: 'Inspector(s)',
-    // },
     {
         id: 8,
         isSelected: false,
@@ -638,6 +623,7 @@ const SearchInspection = () => {
 
         setMasterData([...temp]);
     };
+    console.log(searchList,'searchList')
     return (
         <CustomHeader
             title="Search Inspection"
@@ -792,11 +778,12 @@ const SearchInspection = () => {
                                     <View style={{ marginVertical: 10 }} key={item.id}>
                                         <RadioButtonComponent
                                             lable={item.label}
+                                            staticValue={item.label}
                                             value={item.isSelected ? item.label : ''}
                                             onChange={val => {
                                                 setSearchList(pre => {
                                                     let temp = pre.map(item =>
-                                                        item.id === val.id ? { ...item, isSelected: true } : { ...item, isSelected: false },
+                                                        item.id == val.id ? { ...item, isSelected: true } : { ...item, isSelected: false },
                                                     );
                                                     return temp;
                                                 });
