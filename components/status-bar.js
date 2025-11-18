9804ce40b0a900c55863959d91d4b7626b3b98ae
BaluMurugesan24 <bamurugesan@omnex.com>
2025-11-18 14:01:43 +0530

    status bar color updated

--- components/status-bar.js
+++ components/status-bar.js
@@ -4,17 +4,16 @@ import { getStatusBarHeight } from 'react-native-status-bar-height';
 import useTheme from '../theme/useTheme';
 import { Modes, Themes } from '../constants/app-constant';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { COLORS } from 'constants/theme-constants';
 
 const StatusBarAndroidIOS = ({ backgroundColor = null, animated = true }) => {
     const { theme } = useTheme();
     const insets = useSafeAreaInsets();
 
     return (
-        <View style={[Platform.OS === 'ios' ? { height: getStatusBarHeight() }:{paddingTop: insets.top,backgroundColor:COLORS.tabtheme}]}>
+        <View style={[Platform.OS === 'ios' ? { height: getStatusBarHeight() }:{paddingTop: insets.top,backgroundColor:'#a09f9f'}]}>
             <StatusBar
                 backgroundColor={backgroundColor || theme.mode.backgroundColor}
-                barStyle={theme.selectedMode === Modes.light ? 'dark-content' : 'light-content'}
+                barStyle={theme.selectedMode === Modes.light ? 'light-content':'dark-content' }
                 animated={animated}
             />
         </View>
