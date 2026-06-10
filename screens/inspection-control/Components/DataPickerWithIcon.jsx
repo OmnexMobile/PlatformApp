2a4083576456284fa86f0f05294ee2e139b3fb67
parveen <pbanu@omnex.com>
2026-06-10 10:03:40 +0530

    ic

--- screens/inspection-control/Components/DataPickerWithIcon.jsx
+++ screens/inspection-control/Components/DataPickerWithIcon.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 import IconE from 'react-native-vector-icons/Fontisto';
-import DatePicker from 'react-native-date-picker';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import moment from 'moment';
 
 const DataPickerWithIcon = ({
@@ -74,9 +74,8 @@ const DataPickerWithIcon = ({
             </TouchableOpacity>
 
             {editable && (
-                <DatePicker
-                    modal
-                    open={showPicker}
+                <DateTimePickerModal
+                    isVisible={showPicker}
                     date={date}
                     mode={type === 'date' ? 'date' : 'time'}
                     onConfirm={handleConfirm}
